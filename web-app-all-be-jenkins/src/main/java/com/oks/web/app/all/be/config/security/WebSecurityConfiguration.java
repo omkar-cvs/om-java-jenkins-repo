2d531cfeeeba80f00ce0d547142c4865e16f18aa
Omkar Singh <omkarsinghaws@gmail.com>
2024-04-28 01:02:33 +0530

    correcting url

--- web-app-all-be-jenkins/src/main/java/com/oks/web/app/all/be/config/security/WebSecurityConfiguration.java
+++ web-app-all-be-jenkins/src/main/java/com/oks/web/app/all/be/config/security/WebSecurityConfiguration.java
@@ -40,7 +40,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors();
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/WebAppJenkinsJob/authenticate", "/user/registerNewUser","/role/save").permitAll()
+                .authorizeRequests().antMatchers("/authenticate", "/user/registerNewUser","/role/save").permitAll()
                 .antMatchers(HttpHeaders.ALLOW).permitAll()
                 .anyRequest().authenticated()
                 .and()
